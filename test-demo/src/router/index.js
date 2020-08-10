67bc23b769831b1c67b857cd03ead41fc384be1a
hyron_anpf <anpf@hyron-js.com>
2020-08-10 17:56:41 +0800

    添加导航栏和library页面

--- test-demo/src/router/index.js
+++ test-demo/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import AppIndex from '@/components/home/AppIndex'
 import Login from '@/components/Login'
+import Home from '@/components/Home'
+import LibraryIndex from '../components/library/LibraryIndex'
 
 Vue.use(Router)
 
@@ -14,17 +16,34 @@ export default new Router({
     //   component: HelloWorld
     // }
     {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: '/library',
+          name: 'Libarary',
+          component: LibraryIndex,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
+    },
+
+    {
       path: '/login',
       name: 'Login',
       component: Login
-    },
-    {
-      path: '/index',
-      name: 'AppIndex',
-      component: AppIndex,
-      meta: {
-        requireAuth: true
-      }
     }
   ]
 })
